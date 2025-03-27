f044ae539a8a72bd5dacee22f2dd87c4cf97d58c
hurley04155@gmail.com <hurley04155@gmail.com>
2025-03-27 16:48:00 -0400

    Added beginning JavaScript interaction

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
 "use strict";
 
 const projectDisplay = document.querySelector(".projects");
-const addNew = document.getElementById("new-btn");
+const addNewBtn = document.getElementById("new-btn");
 const modal = document.getElementById("modal");
-const closeModal = document.getElementById("close-modal");
+const closeModalBtn = document.getElementById("close-modal");
+const saveBtn = document.getElementById("save-btn");
+const taskTitle = document.getElementById("title");
+const description = document.getElementById("description");
+const dueDate = document.getElementById("due-date");
+const isPriority = document.querySelector("input[name='priority']:checked");
 
-function Project(title, description, dueDate, priority) {
+addNewBtn.addEventListener("click", () => modal.showModal());
+closeModalBtn.addEventListener("click", () => modal.close());
+saveBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const { title, descrip, date, priority } = new Project(
+    taskTitle.value,
+    description.value,
+    dueDate.value
+    // isPriority.value
+  );
+  // console.log(priority);
+  const newDiv = document.createElement("div");
+  newDiv.classList.add("task-box");
+  projectDisplay.append(newDiv);
+  newDiv.innerHTML = `
+  <p>${title}</p>
+  <hr>
+  <p>${descrip}</p>
+  <p>${date}</p>
+  `;
+});
+
+function Project(title, descrip, date, priority) {
   this.title = title;
-  this.description = description;
-  this.dueDate = dueDate;
+  this.descrip = descrip;
+  this.date = date;
   this.priority = priority;
 }
-
-const project1 = new Project("History Paper", "Begin researching Greece");
-
-addNew.addEventListener("click", () => modal.showModal());
-closeModal.addEventListener("click", () => modal.close());
